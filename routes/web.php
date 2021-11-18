eb1a6b67ba54aa9650f55bbd3bc6b506cf699a6d
Dejan <38137502+Dejan1990@users.noreply.github.com>
2021-11-18 14:48:48 +0100

    Idea Model and Migrations

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,9 @@
 <?php
 
+use App\Http\Controllers\IdeaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
-
-Route::view('/idea', 'show');
+Route::get('/', [IdeaController::class, 'index'])->name('idea.index');
+Route::get('/ideas/{idea:slug}', [IdeaController::class, 'show'])->name('idea.show');
 
 require __DIR__.'/auth.php';
