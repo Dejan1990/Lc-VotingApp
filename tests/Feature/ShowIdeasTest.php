fd8e7a0058476d723c29876a8044df17bc129833
Dejan <38137502+Dejan1990@users.noreply.github.com>
2021-11-19 17:10:58 +0100

    Voting Database Structure

--- tests/Feature/ShowIdeasTest.php
+++ tests/Feature/ShowIdeasTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Models\Idea;
+use App\Models\User;
 use App\Models\Status;
 use App\Models\Category;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -16,6 +17,8 @@ class ShowIdeasTest extends TestCase
     /** @test */
     public function list_of_ideas_shows_on_main_page()
     {
+        $user = User::factory()->create();
+
         $categoryOne = Category::factory()->create([
             'name' => 'Category 1'
         ]);
@@ -28,6 +31,7 @@ class ShowIdeasTest extends TestCase
         $statusConsidering = Status::factory()->create(['name' => 'Considering', 'classes' => 'bg-purple text-white']);
 
         $ideaOne = Idea::factory()->create([
+            'user_id' => $user->id,
             'title' => 'My first idea',
             'category_id' => $categoryOne->id,
             'status_id' => $statusOpen->id,
@@ -35,6 +39,7 @@ class ShowIdeasTest extends TestCase
         ]);
 
         $ideaTwo = Idea::factory()->create([
+            'user_id' => $user->id,
             'title' => 'My second idea',
             'category_id' => $categorytwo->id,
             'status_id' => $statusConsidering->id,
@@ -59,12 +64,15 @@ class ShowIdeasTest extends TestCase
     /** @test */
     public function single_idea_shows_correctly_on_the_show_page()
     {
+        $user = User::factory()->create();
+
         $categoryOne = Category::factory()->create(['name' => 'Category 1']);
         $categoryTwo = Category::factory()->create(['name' => 'Category 2']);
 
         $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
 
         $idea1 = Idea::factory()->create([
+            'user_id' => $user->id,
             'category_id' => $categoryOne->id,
             'status_id' => $statusOpen->id,
             'title' => 'My first idea',
@@ -72,6 +80,7 @@ class ShowIdeasTest extends TestCase
         ]);
 
         $idea2 = Idea::factory()->create([
+            'user_id' => $user->id,
             'category_id' => $categoryTwo->id,
             'status_id' => $statusOpen->id,
             'title' => 'My second idea'
@@ -91,31 +100,28 @@ class ShowIdeasTest extends TestCase
     /** @test */
     public function ideas_pagination_works()
     {
+        $user = User::factory()->create();
+
         $categoryOne = Category::factory()->create(['name' => 'Category 1']);
 
         $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
 
         Idea::factory(Idea::PAGINATION_COUNT + 1)->create([
+            'user_id' => $user->id,
             'category_id' => $categoryOne->id,
             'status_id' => $statusOpen->id,
         ]);
-
         $ideaOne = Idea::find(1);
         $ideaOne->title = 'My First Idea';
         $ideaOne->save();
-
         $ideaEleven = Idea::find(11);
         $ideaEleven->title = 'My Eleventh Idea';
         $ideaEleven->save();
-
         $response = $this->get('/');
-
-        $response->assertSee($ideaOne->title);
-        $response->assertDontSee($ideaEleven->title);
-
-        $response = $this->get('/?page=2');
-
         $response->assertSee($ideaEleven->title);
         $response->assertDontSee($ideaOne->title);
+        $response = $this->get('/?page=2');
+        $response->assertSee($ideaOne->title);
+        $response->assertDontSee($ideaEleven->title);
     }
 }
