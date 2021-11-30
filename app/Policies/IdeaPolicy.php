58c74cde4fc216b8f00e802ae5e1e8ee36007b35
Dejan <38137502+Dejan1990@users.noreply.github.com>
2021-11-30 13:21:43 +0100

    Delete Idea

--- app/Policies/IdeaPolicy.php
+++ app/Policies/IdeaPolicy.php
@@ -68,7 +68,7 @@ class IdeaPolicy
      */
     public function delete(User $user, Idea $idea)
     {
-        //
+        return $user->id === (int) $idea->user_id || $user->isAdmin();
     }
 
     /**
