1f5fd318e9ff4b6d14fb567f65113ea89ff9b86a
Dejan <38137502+Dejan1990@users.noreply.github.com>
2021-12-01 16:38:33 +0100

    Success Message Notification Works

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -93,6 +93,13 @@
             </div>
         </main>
 
+        @if (session('success_message'))
+            <x-notification-success
+                :redirect="true"
+                message-to-display="{{ (session('success_message')) }}"
+            />
+        @endif
+
         {{-- @stack('modals') da ne ucitava, ovo bismo koristili --}}
         @livewireScripts
     </body>
