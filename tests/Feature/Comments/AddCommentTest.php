2ba60a8bca0efcb1d97f0af9ce56bc15c2667bee
Dejan <38137502+Dejan1990@users.noreply.github.com>
2021-12-03 20:22:12 +0100

    Show and Add Comments Tests

---
+++ tests/Feature/Comments/AddCommentTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Tests\Feature\Comments;
+
+use App\Http\Livewire\AddComment;
+use App\Models\Comment;
+use App\Models\Idea;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class AddCommentTest extends TestCase
+{
+    use RefreshDatabase;
+
+     /*You don't need to create users alongside the idea factory, because you can use the existing relationship with the idea model ($idea->user), as its factory creates a user for you.*/
+
+    /** @test */
+    public function add_comment_livewire_component_render()
+    {
+        $idea = Idea::factory()->create();
+
+        $this->get(route('idea.show', $idea))
+            ->assertSeeLivewire('add-comment');
+    }
+
+    /** @test */
+    public function add_comment_form_renders_when_user_is_logged_in()
+    {
+        //$user = User::factory()->create();
+        $idea = Idea::factory()->create();
+
+        $this->actingAs($idea->user)
+            ->get(route('idea.show', $idea))
+            ->assertSee('Share your thoughts');
+    }
+
+    /** @test */
+    public function add_comment_form_does_not_render_when_user_is_logged_out()
+    {
+        $idea = Idea::factory()->create();
+
+        $this->get(route('idea.show', $idea))
+            ->assertDontSee('Share your thoughts');
+    }
+
+    /** @test */
+    public function add_comment_form_validation_works()
+    {
+        $user = User::factory()->create();
+        $idea = Idea::factory()->create();
+
+        Livewire::actingAs($user)
+            ->test(AddComment::class, [
+                'idea' => $idea
+            ])
+            ->set('comment', '')
+            ->call('addComment')
+            ->assertHasErrors('comment')
+            ->set('comment', 'ab')
+            ->assertHasErrors('comment');
+    }
+
+    /** @test */
+    public function add_comment_form__works()
+    {
+        $user = User::factory()->create();
+        $idea = Idea::factory()->create();
+
+        Livewire::actingAs($user)
+            ->test(AddComment::class, [
+                'idea' => $idea
+            ])
+            ->set('comment', 'My first comment')
+            ->call('addComment')
+            ->assertEmitted('commentWasAdded');
+
+        $this->assertEquals(1, Comment::count());
+        $this->assertEquals('My first comment', $idea->comments->first()->body);
+    }
+
+        
+}
---
+++ tests/Feature/Comments/ShowCommentsTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Tests\Feature\Comments;
+
+use Tests\TestCase;
+use App\Models\Idea;
+use App\Models\User;
+use App\Models\Comment;
+use App\Http\Livewire\IdeaComment;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ShowCommentsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function idea_comments_livewire_component_renders()
+    {
+        $idea = Idea::factory()->create();
+        Comment::factory()->create();
+
+        $this->get(route('idea.show', $idea))
+            ->assertSeeLivewire('idea-comments');
+    }
+
+    /** @test */
+    public function idea_comment_livewire_component_renders()
+    {
+        $idea = Idea::factory()->hasComments()->create();
+
+        $this->get(route('idea.show', $idea))
+            ->assertSeeLivewire('idea-comment');
+    }
+
+    /** @test */
+    public function no_comments_shows_appropriate_message()
+    {
+        $idea = Idea::factory()->create();
+
+        $this->get(route('idea.show', $idea))
+            ->assertSee('No comments yet...');
+    }
+
+    /** @test */
+    public function list_of_comments_shows_on_idea_page()
+    {
+        /*$idea = Idea::factory()->hasComments(5)->create();
+        
+        $this->get(route('idea.show', $idea))
+            ->assertViewHas('idea', function ($idea) {
+                return $idea->comments->count() === 5
+                    && $idea->comments->first()->id === 1
+                    && $idea->comments->get(2)->id === 3
+                    && $idea->comments->last()->id === 5;
+            })
+            ->assertSee('5 comments');*/
+
+        $idea = Idea::factory()->hasComments(5)->create();
+
+        $this->get(route('idea.show', $idea))
+            ->assertSee('5 comments');
+        
+        $this->assertTrue($idea->comments->first()->id === 1);
+        $this->assertFalse($idea->comments->first()->id === 5);
+        $this->assertTrue($idea->comments->get(1)->id === 2);
+        $this->assertTrue($idea->comments->last()->id === 5);
+    }
+
+    /** @test */
+    public function comments_count_shows_correctly_on_index_page()
+    {
+        Idea::factory()->hasComments(5)->create();
+        //Comment::factory()->count(5)->forIdea()->create();
+
+        $this->get('/')
+            ->assertSee('5 comments');
+    }
+
+    /** @test */
+    public function op_badge_shows_if_author_of_idea_comments_on_idea()
+    {
+        $user = User::factory()->create();
+        $idea = Idea::factory()->create(['user_id' => $user->id]);
+
+        Comment::factory()->create([
+            'user_id' => $user->id,
+            'idea_id' => $idea->id
+        ]);
+
+        $this->get(route('idea.show', $idea))
+            ->assertSee('OP');
+    }
+
+    /** @test */
+    public function comments_pagination_works()
+    {
+        $idea = Idea::factory()->create();
+
+        $commentOne = Comment::factory()->create([
+            'idea_id' => $idea
+        ]);
+
+        Comment::factory($commentOne->getPerPage())->create([
+            'idea_id' => $idea->id,
+        ]);
+
+        $response = $this->get(route('idea.show', $idea));
+
+        $response->assertSee($commentOne->body);
+        $response->assertDontSee(Comment::find(Comment::count())->body);
+
+        $response = $this->get(route('idea.show', [
+            'idea' => $idea,
+            'page' => 2,
+        ]));
+
+        $response->assertDontSee($commentOne->body);
+        $response->assertSee(Comment::find(Comment::count())->body);
+    }
+}
